8a72883262f2dc90ba745f333bbaa5643a751125
Hforna <henriqueflashpingo@gmail.com>
2025-10-18 18:34:34 -0300

    configure infrastructure dependecies and ef core context

---
+++ InvoiceAnalyzer.Api/Controllers/InvoicesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceAnalyzer.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InvoicesController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> UploadInvoice([FromForm]IFormFile invoice)
+    {
+        
+    }
+}
---
+++ InvoiceAnalyzer.Infrastructure/DependencyInjectionConfigs.cs
@@ -0,0 +1,19 @@
+﻿using InvoiceAnalyzer.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceAnalyzer.Infrastructure;
+
+public static class DependencyInjectionConfigs
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        Persistence(services, configuration);
+    }
+
+    static void Persistence(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DataContext>(d => d.UseSqlServer(configuration.GetConnectionString("sqlserver")));
+    }
+}
